007979d55cf2c249ffa2c8fec466f40f1fe4a777
zas_syx <yuxin.song@zasemicon.com>
2024-08-06 18:07:16 +0800

    脚本增加golden+multi boot mcs操作

--- generate_bin.bat
+++ generate_bin.bat
@@ -5,12 +5,14 @@ set scriptDir=%~dp0
 
 call vivado -mode tcl -nojournal -nolog -source pcie_write_cfgmem.tcl
 
-xcopy  %scriptDir%project_1\project_1.runs\impl_1\pcie_card_top.bit  %scriptDir%version_bin\pcie_v2_version\ 
-xcopy  %scriptDir%project_1\project_1.runs\impl_1\pcie_card_top.ltx  %scriptDir%version_bin\pcie_v2_version\
-xcopy  %scriptDir%project_1\project_1.runs\impl_1\pcie_card_top.bin  %scriptDir%version_bin\pcie_v2_version\
+xcopy  %scriptDir%project_1\project_1.runs\impl_1\pcie_card_top.bit  %scriptDir%version_bin\
+xcopy  %scriptDir%project_1\project_1.runs\impl_1\pcie_card_top.ltx  %scriptDir%version_bin\
+xcopy  %scriptDir%project_1\project_1.runs\impl_1\pcie_card_top.bin  %scriptDir%version_bin\
+xcopy  %scriptDir%project_1\project_1.runs\impl_1\pcie_card_top.mcs  %scriptDir%version_bin\
 
-ren  %scriptDir%version_bin\pcie_v2_version\pcie_card_top.bit PCG1_PCIE_v%version%.bit 
-ren  %scriptDir%version_bin\pcie_v2_version\pcie_card_top.ltx PCG1_PCIE_v%version%.ltx 
-ren  %scriptDir%version_bin\pcie_v2_version\pcie_card_top.bin PCG1_PCIE_v%version%.bin 
+ren  %scriptDir%version_bin\pcie_card_top.bit PCG1_PCIE_v%version%.bit 
+ren  %scriptDir%version_bin\pcie_card_top.ltx PCG1_PCIE_v%version%.ltx 
+ren  %scriptDir%version_bin\pcie_card_top.bin PCG1_PCIE_v%version%.bin 
+ren  %scriptDir%version_bin\pcie_card_top.mcs PCG1_PCIE_v%version%.mcs 
 
 pause
--- pcie_write_cfgmem.tcl
+++ pcie_write_cfgmem.tcl
@@ -1,2 +1,3 @@
 write_cfgmem -force -format bin -size 128 -interface BPIx16 -loadbit "up 0x00000000 ./project_1/project_1.runs/impl_1/pcie_card_top.bit" ./project_1/project_1.runs/impl_1/pcie_card_top
+write_cfgmem  -format mcs -size 64 -interface BPIx16 -loadbit {up 0x00000000 "./version_bin/PCG_PCIE_gloden.bit" up 0x00400000 "./project_1/project_1.runs/impl_1/pcie_card_top.bit" } -checksum -force -file "./project_1/project_1.runs/impl_1/pcie_card_top.mcs"
 exit
